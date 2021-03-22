2ebe1bcc68056a6d23d835455ce606bca23dccb5
Alekearl <alexandrhurko@gmail.com>
2021-03-22 15:44:58 +0200

    checkstyle correction

--- src/main/java/com/http/program/uploadcsv/controller/InformationController.java
+++ src/main/java/com/http/program/uploadcsv/controller/InformationController.java
@@ -3,12 +3,11 @@ package com.http.program.uploadcsv.controller;
 import com.http.program.uploadcsv.model.Information;
 import com.http.program.uploadcsv.service.InformationService;
 import java.util.List;
+import javax.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.websocket.server.PathParam;
 
 @RestController
 @RequestMapping("/information")
@@ -26,7 +25,8 @@ public class InformationController {
     }
 
     @GetMapping("/all-departments/highest-salary")
-    public List<Information> getHighestSalaryInDepartments(@PathParam("department") String department) {
+    public List<Information> getHighestSalaryInDepartments(@PathParam("department")
+                                                                       String department) {
         return informationService.getAllWithHighestDepartmentSalary(department);
     }
 
--- src/main/java/com/http/program/uploadcsv/repository/InformationRepository.java
+++ src/main/java/com/http/program/uploadcsv/repository/InformationRepository.java
@@ -9,6 +9,7 @@ public interface InformationRepository extends JpaRepository<Information, Long> 
     @Query("FROM Information i WHERE i.department = :department ORDER BY i.salary DESC")
     List<Information> getAllEmployeeWithHighestSalaryInDepartment(String department);
 
-    @Query("FROM Information i WHERE i.employee <> '' AND i.employee IS NOT NULL ORDER By i.department, i.salary DESC")
+    @Query("FROM Information i WHERE i.employee <> '' "
+            + "AND i.employee IS NOT NULL ORDER By i.department, i.salary DESC")
     List<Information> findAllByDepartmentAndEmployeeContainsOrderBySalaryDesc();
 }
